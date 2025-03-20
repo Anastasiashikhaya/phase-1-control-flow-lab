81598e1705bcdac19e28baf53585bdc72c97308f
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-03-20 19:01:13 +0300

    Anna control flow

---
+++ index_old.js
@@ -0,0 +1,16 @@
+scuberGreetingForFeet(){
+  // Write your code here!
+ 
+  
+
+
+function ternaryCheckCity(){
+  // Write your code here!
+  
+
+
+
+
+function switchOnCharmFromTip(){
+  // Write your code here!
+
